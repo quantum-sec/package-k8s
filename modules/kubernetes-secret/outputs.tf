5d136ea0679e09acca7f8ea45bb8bc2d632963d3
Luke Fritz <lukefritz@gmail.com>
2020-08-26 17:34:48 -0500

    QSE-131: Fix incorrect value accessor in outputs (fix)

--- modules/kubernetes-secret/outputs.tf
+++ modules/kubernetes-secret/outputs.tf
@@ -1,19 +1,19 @@
 output "generation" {
   description = "A sequence number representing the specific generation of the desired state."
-  value       = kubernetes_secret.secret.metadata.generation
+  value       = kubernetes_secret.secret.metadata[0].generation
 }
 
 output "resource_version" {
   description = "An opaque value that represents the internal version of this secret that can be used by clients to determine when the secret has changed."
-  value       = kubernetes_secret.secret.metadata.resource_version
+  value       = kubernetes_secret.secret.metadata[0].resource_version
 }
 
 output "self_link" {
   description = "A URL representing this secret."
-  value       = kubernetes_secret.secret.metadata.self_link
+  value       = kubernetes_secret.secret.metadata[0].self_link
 }
 
 output "uid" {
   description = "The unique identifier of this secret."
-  value       = kubernetes_secret.secret.metadata.uid
+  value       = kubernetes_secret.secret.metadata[0].uid
 }
--- modules/kubernetes-service/outputs.tf
+++ modules/kubernetes-service/outputs.tf
@@ -1,21 +1,21 @@
 output "generation" {
   description = "A sequence number representing a specific generation of the desired state."
-  value       = kubernetes_service.service.metadata.generation
+  value       = kubernetes_service.service.metadata[0].generation
 }
 
 output "resource_version" {
   description = "An opaque value that represents the internal version of this service that can be used by clients to determine when the service has changed."
-  value       = kubernetes_service.service.metadata.resource_version
+  value       = kubernetes_service.service.metadata[0].resource_version
 }
 
 output "self_link" {
   description = "A URL representing this service."
-  value       = kubernetes_service.service.metadata.self_link
+  value       = kubernetes_service.service.metadata[0].self_link
 }
 
 output "uid" {
   description = "The unique identifier for this service."
-  value       = kubernetes_service.service.metadata.uid
+  value       = kubernetes_service.service.metadata[0].uid
 }
 
 output "load_balancer_ingress_ip" {
