254553ca2462872af775e4527ce77774ddca8caa
Chris Jaimon <chris.jaimon@quantum.security>
2023-08-17 13:09:48 +0530

    XDR-4399: Bump terraform version to 1.2.5

--- modules/kubernetes-persistent-volume/outputs.tf
+++ modules/kubernetes-persistent-volume/outputs.tf
@@ -1,4 +1,4 @@
 output "name" {
   description = "The name of the persistent volume."
-  value       = kubernetes_persistent_volume.volume.metadata.0.name
+  value       = kubernetes_persistent_volume.volume.metadata[0].name
 }
